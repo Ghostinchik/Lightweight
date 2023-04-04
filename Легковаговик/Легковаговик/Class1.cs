2460e8ef65bfba0406ece24b74a307fec4778ff4
Sasha <117905137+Ghostinchik@users.noreply.github.com>
2023-04-04 16:15:44 +0300

    Lightweight

---
+++ Легковаговик/Легковаговик/Class1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Легковаговик
+{
+    internal class Class1
+    {
+        public class Ball
+        {
+            public int Type { get; set; }
+            public RectangleF Bounds { get; set; }
+
+            public Ball(int type, RectangleF bounds)
+            {
+                Type = type;
+                Bounds = bounds;
+            }
+        }
+
+        public class GameBoard
+        {
+            private Ball[,] balls;
+
+            public GameBoard(int size)
+            {
+                balls = new Ball[size, size];
+            }
+
+            public void SetBall(int x, int y, int type, float ballSize)
+            {
+                var ballBounds = new RectangleF(x * ballSize, y * ballSize, ballSize, ballSize);
+                balls[x, y] = new Ball(type, ballBounds);
+            }
+
+            public Ball GetBall(int x, int y)
+            {
+                return balls[x, y];
+            }
+
+            public void ClearBall(int x, int y)
+            {
+                balls[x, y] = null;
+            }
+
+            public void ClearAllBalls()
+            {
+                balls = new Ball[balls.GetLength(0), balls.GetLength(1)];
+            }
+        }
+
+        public class GameBoardGenerator
+        {
+            private Random random = new Random();
+
+            public GameBoard Generate(int size, float ballSize)
+            {
+                var gameBoard = new GameBoard(size);
+
+                for (int x = 0; x < size; x++)
+                {
+                    for (int y = 0; y < size; y++)
+                    {
+                        int type = random.Next(1, 6);
+                        gameBoard.SetBall(x, y, type, ballSize);
+                    }
+                }
+
+                return gameBoard;
+            }
+        }
+
+
+    }
+}
---
+++ Легковаговик/Легковаговик/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static Легковаговик.Class1;
+
+namespace Легковаговик
+{
+    public partial class Form1 : Form
+    {
+
+        private const int GameBoardSize = 8;
+        private const float BallSize = 50;
+
+        private GameBoardGenerator gameBoardGenerator = new GameBoardGenerator();
+        private GameBoard gameBoard;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            gameBoard = gameBoardGenerator.Generate(GameBoardSize, BallSize);
+            Invalidate();
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+            for (int x = 0; x < GameBoardSize; x++)
+            {
+                for (int y = 0; y < GameBoardSize; y++)
+                {
+                    var ball = gameBoard.GetBall(x, y);
+                    if (ball != null)
+                    {
+                        e.Graphics.FillEllipse(GetBallBrush(ball.Type), ball.Bounds);
+                    }
+                }
+            }
+        }
+        
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                int x = e.X / (int)BallSize;
+                int y = e.Y / (int)BallSize;
+
+                if (gameBoard.GetBall(x, y) == null)
+                {
+                    gameBoard.SetBall(x, y, 1, BallSize);
+                    Invalidate();
+                }
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                int x = e.X / (int)BallSize;
+                int y = e.Y / (int)BallSize;
+
+                if (gameBoard.GetBall(x, y) != null)
+                {
+                    gameBoard.ClearBall(x, y);
+                    Invalidate();
+                }
+            }
+        }
+        private Brush GetBallBrush(int type)
+        {
+            switch (type)
+            {
+                case 1:
+                    return Brushes.Red;
+                case 2:
+                    return Brushes.Green;
+                case 3:
+                    return Brushes.Blue;
+                case 4:
+                    return Brushes.Yellow;
+                case 5:
+                    return Brushes.Purple;
+                default:
+                    throw new ArgumentException($"Неправильний тип кульки: {type}");
+            }
+        }
+    }
+}
